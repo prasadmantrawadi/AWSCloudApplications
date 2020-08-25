7e6432164ab245b613a02fbd5528e18257e90553
pmantrawadi <v-pmantrawadi@revspringinc.com>
2020-08-25 19:20:51 +0530

    second microservice with cognito auth

--- patientservice/src/main/java/com/prasad/samples/patient/controller/PatientController.java
+++ patientservice/src/main/java/com/prasad/samples/patient/controller/PatientController.java
@@ -1,18 +1,27 @@
 package com.prasad.samples.patient.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PatientController {
 
+	private final static Logger logger = LoggerFactory.getLogger(PatientController.class);
 	
-	@GetMapping(value = "/get-data")
-	public String getData(HttpServletRequest  request, HttpServletResponse response) {
-		System.out.println("Inside Patient Service getData method");
-		return "Hello from Patient Service getData method!!";
+	@GetMapping(value = "/patients")
+	public String getAllPatients(@AuthenticationPrincipal Jwt jwt) {
+		logger.info("Inside Patient Service getAllPatients method");
+		logger.info("Inside Doctor Service getData method");
+		logger.info("***** JWT Headers: {}", jwt.getHeaders());
+		logger.info("***** JWT Claims: {}", jwt.getClaims().toString());
+		logger.info("***** JWT Token: {}", jwt.getTokenValue());
+		logger.info(String.format("Patient Resource accessed by: %s (with subjectId: %s)" ,
+	            jwt.getClaims().get("username"),
+	            jwt.getSubject()));
+		return "All Patients details from Patients Service";
 	}
 }
